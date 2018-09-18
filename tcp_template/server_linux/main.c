af2b6290fcb48e1fc69b6f811bd0b2531dae79c8
MrSandman5 <mrsandman138@gmail.com>
2018-09-18 23:32:27 +0300

    Connection multithreading and socket shutdown

--- tcp_template/server_linux/main.c
+++ tcp_template/server_linux/main.c
@@ -4,32 +4,22 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#include <errno.h>
-#include <unistd.h>
+#include <arpa/inet.h>
 #include <string.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <pthread.h>
+
+void *connection_handler(void *);
 
 int main(int argc, char *argv[]) {
-    int opt = 1;
-    int sockfd, newsockfd, client_socket[30], max_clients = 30, activity, i, valread, sd, max_sd;
+    int sockfd, newsockfd;
     uint16_t portno;
     unsigned int clilen;
-    char buffer[256];
     struct sockaddr_in serv_addr, cli_addr;
-    ssize_t n;
-    fd_set readfds; 
-
-    for(i = 0; i < max_clients; i++)
-    {
-	client_socket[i] = 0;
-    }
 
     /* First call to socket() function */
 
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    {
         perror("ERROR opening socket");
         exit(1);
     }
@@ -38,18 +28,13 @@ int main(int argc, char *argv[]) {
     bzero((char *) &serv_addr, sizeof(serv_addr));
     portno = 5001;
 
-    if( setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(int)))
-    {
-	perror("setsockopt");
-	exit(EXIT_FAILURE);
-    }
-
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
 
     /* Now bind the host address using bind() call.*/
-    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+    {
         perror("ERROR on binding");
         exit(1);
     }
@@ -63,145 +48,63 @@ int main(int argc, char *argv[]) {
 	perror("listen");
 	exit(EXIT_FAILURE);
     }
+    puts("Waiting for incoming connections");
     clilen = sizeof(cli_addr);
 
-    while(1)   
-    {   
-        //clear the socket set  
-        FD_ZERO(&readfds);   
-     
-        //add master socket to set  
-        FD_SET(sockfd, &readfds);   
-        max_sd = sockfd;   
-             
-        //add child sockets to set  
-        for ( i = 0 ; i < max_clients ; i++)   
-        {   
-            //socket descriptor  
-            sd = client_socket[i];   
-                 
-            //if valid socket descriptor then add to read list  
-            if(sd > 0)   
-                FD_SET( sd , &readfds);   
-                 
-            //highest file descriptor number, need it for the select function  
-            if(sd > max_sd)   
-                max_sd = sd;   
-        }   
-     
-        //wait for an activity on one of the sockets , timeout is NULL ,  
-        //so wait indefinitely  
-        activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);   
-       
-        if ((activity < 0) && (errno!=EINTR))   
-        {   
-            printf("select error");   
-        }   
-             
-        //If something happened on the master socket ,  
-        //then its an incoming connection  
-        if (FD_ISSET(sockfd, &readfds))   
-        {   
-            newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    while( (newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen)))
+    {
+	puts("Connection accepted");
 
-    	    if (newsockfd < 0) {
-        	perror("ERROR on accept");
-        	exit(1);
-    	    }
+	pthread_t sn_thread;
 
-    	/* If connection is established then start communicating */
-    	    bzero(buffer, 256);
-            n = read(newsockfd, buffer, 255); // recv on Windows
+	if( pthread_create( &sn_thread, NULL, connection_handler, &newsockfd) < 0)
+	{
+	    perror("Could not create thread");
+	    return 1;
+	}
 
-            if (n < 0) {
-        	perror("ERROR reading from socket");
-        	exit(1);
-    	    }
-
-            printf("Here is the message: %s\n", buffer);
-
-    	/* Write a response to the client */
-    	    n = write(newsockfd, "I got your message", 18); // send on Windows
-
-            if (n < 0) {
-        	perror("ERROR writing to socket");
-        	exit(1);
-    	     }
-             
-                 
-            //add new socket to array of sockets  
-            for (i = 0; i < max_clients; i++)   
-            {   
-                //if position is empty  
-                if( client_socket[i] == 0 )   
-                {   
-                    client_socket[i] = newsockfd;   
-                    printf("Adding to list of sockets as %d\n" , i);   
-                         
-                    break;   
-                }   
-            }   
-        }   
-             
-        //else its some IO operation on some other socket 
-        for (i = 0; i < max_clients; i++)   
-        {   
-            sd = client_socket[i];   
-                 
-            if (FD_ISSET( sd , &readfds))   
-            {   
-                //Check if it was for closing , and also read the  
-                //incoming message  
-                if ((valread = read( sd , buffer, 256)) == 0)   
-                {   
-                    //Somebody disconnected , get his details and print  
-                    getpeername(sd , (struct sockaddr*)&serv_addr , \ 
-                        (socklen_t*)&clilen);   
-                    printf("Host disconnected , ip %s , port %d \n" ,  
-                          inet_ntoa(serv_addr.sin_addr) , ntohs(serv_addr.sin_port));   
-                         
-                    //Close the socket and mark as 0 in list for reuse  
-                    close(sd);   
-                    client_socket[i] = 0;   
-                }   
-                     
-                //Echo back the message that came in  
-                else 
-                {   
-                    //set the string terminating NULL byte on the end  
-                    //of the data read  
-                    buffer[valread] = '\0';   
-                    send(sd , buffer , strlen(buffer) , 0 );   
-                }   
-            }   
-        }
-    }   
-
-    /* Accept actual connection from the client */
-    /*newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-
-    if (newsockfd < 0) {
-        perror("ERROR on accept");
-        exit(1);
+	puts("Handler assigned");
     }
 
-    /* If connection is established then start communicating */
-    /*bzero(buffer, 256);
-    n = read(newsockfd, buffer, 255); // recv on Windows
+    if (sockfd < 0)
+    {
+	perror("accept failed");
+	return 1;
+    }
+
+    close(sockfd);
+    shutdown(sockfd, 2);
+    return 0;
+}
+
+void *connection_handler(void *socket_desc)
+{
+    //Get the socket descriptor
+    int sock = *(int*)socket_desc;
+    char buffer[256];
+    ssize_t n;
+
+    bzero(buffer, 256);
+    n = read(sock, buffer, 255);
 
     if (n < 0) {
         perror("ERROR reading from socket");
         exit(1);
+	pthread_exit(1);
     }
 
     printf("Here is the message: %s\n", buffer);
 
     /* Write a response to the client */
-    /*n = write(newsockfd, "I got your message", 18); // send on Windows
+    n = write(sock, "I got your message", 18); // send on Windows
 
     if (n < 0) {
         perror("ERROR writing to socket");
         exit(1);
-    }*/
+	pthread_exit(1);
+    }
+    close(sock);
+    shutdown(sock, 2);
+    pthread_exit(0);
     return 0;
 }
