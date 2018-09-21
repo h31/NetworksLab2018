a9ba40520b6f53ecc435e2baad4a01f977147c9b
Pox3R <rombl497@gmail.com>
2018-09-21 23:49:50 +0300

    Server doesn't turn off after msg. Server can service more than one client. Add multithreading

--- tcp_template/client_linux/main.c
+++ tcp_template/client_linux/main.c
@@ -4,11 +4,22 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <unistd.h>
-
+#include <pthread.h>
 #include <string.h>
+#include <signal.h>
+
+int sockfd;
+
+void closeApp() {
+	printf("Closing socket\r\n");
+	shutdown(sockfd, SHUT_RDWR);
+	close(sockfd);
+	exit(0);
+}
 
 int main(int argc, char *argv[]) {
-    int sockfd, n;
+	signal(SIGINT,closeApp);
+    int n;
     uint16_t portno;
     struct sockaddr_in serv_addr;
     struct hostent *server;
@@ -51,28 +62,32 @@ int main(int argc, char *argv[]) {
     /* Now ask for a message from the user, this message
        * will be read by server
     */
+    while(1){
+	    printf("Please enter the message: ");
+	    bzero(buffer, sizeof(buffer));
+	    fgets(buffer, sizeof(buffer)-1, stdin);
+	    if (strstr(buffer, "\\q") != NULL) {
+			closeApp();
+	    }
+	    /* Send message to the server */
+		n = write(sockfd, buffer, strlen(buffer));
 
-    printf("Please enter the message: ");
-    bzero(buffer, 256);
-    fgets(buffer, 255, stdin);
 
-    /* Send message to the server */
-    n = write(sockfd, buffer, strlen(buffer));
+	    if (n <= 0) {
+			perror("ERROR writing to socket");
+			closeApp();
+	    }
 
-    if (n < 0) {
-        perror("ERROR writing to socket");
-        exit(1);
+	    /* Now read server response */
+	    bzero(buffer, 256);
+	    n = read(sockfd, buffer, 255);
+
+	    if (n <= 0) {
+			perror("ERROR reading from socket");
+			closeApp();
+	    }
+
+	    printf("%s\n", buffer);
     }
-
-    /* Now read server response */
-    bzero(buffer, 256);
-    n = read(sockfd, buffer, 255);
-
-    if (n < 0) {
-        perror("ERROR reading from socket");
-        exit(1);
-    }
-
-    printf("%s\n", buffer);
     return 0;
 }
--- tcp_template/server_linux/main.c
+++ tcp_template/server_linux/main.c
@@ -4,21 +4,55 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <unistd.h>
-
+#include <signal.h>
+#include <pthread.h>
 #include <string.h>
 
+#define OPEN 0
+#define CLOSED 1
+
+void *connection_f ();
+void *connect_handler(void* args);
+
+int sockfd;
+
+int max_clients = 2;
+int all_clients = 0;
+int isClose = OPEN;
+
+void clear_all(){
+	isClose = CLOSED;
+	printf("closing server\r\n");
+	while (all_clients != 0){
+		printf("Waiting for closing clients... \r\n");
+		sleep(1);
+	}
+	printf("Closing server...\n");
+	shutdown(sockfd, SHUT_RDWR);
+	close(sockfd);
+}
+
+void deleteConnection(int newsockfd){
+	printf("Closing socket = %d \r\n", newsockfd);
+	shutdown(newsockfd, SHUT_RDWR);
+	close(newsockfd);
+	all_clients--;
+	pthread_exit(0);
+}
+int sockfd;
+
 int main(int argc, char *argv[]) {
-    int sockfd, newsockfd;
+	//CTRL+C
+	signal(SIGINT, clear_all);
+	
     uint16_t portno;
-    unsigned int clilen;
-    char buffer[256];
-    struct sockaddr_in serv_addr, cli_addr;
-    ssize_t n;
+    struct sockaddr_in serv_addr;
+
 
     /* First call to socket() function */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (sockfd < 0) {
+    if (sockfd <= 0) {
         perror("ERROR opening socket");
         exit(1);
     }
@@ -37,39 +71,81 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    /* Now start listening for the clients, here process will
-       * go in sleep mode and will wait for the incoming connection
-    */
+	/* Making new thread for messaging with client */
+	pthread_t thread;	//thread
+	int result;		//result of thread creating
+	result = pthread_create(&thread, NULL, connection_f, NULL); //create new thread
+	if(result != 0) {
+	    perror("Error while creating thread");
+	}
 
+	char cmd;
+	
+	while(!isClose){
+		cmd = getchar();
+		if (cmd == 'q'){
+			break;
+		}
+	}
+    clear_all();
+    return 0;
+}
+
+void *connection_f (){
+    /* Accept actual connection from the client */
+    struct sockaddr_in cli_addr;
+	unsigned int clilen;
+	int newsockfd;
+    int n;
+	
     listen(sockfd, 5);
     clilen = sizeof(cli_addr);
+	
+	pthread_t threadCreation;
+	
+	while (!isClose) {
+		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+		if (newsockfd <= 0) {
+        	perror("ERROR on accept");
+        	continue;
+    	}
+		n = pthread_create(&threadCreation, NULL, connect_handler, (void*) newsockfd);
+		if (n != 0) {
+			printf("Error on creating client thread");
+		} else {
+			all_clients++;
+		}
+	}
+	
+	pthread_exit(0);
+}
 
-    /* Accept actual connection from the client */
-    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+void *connect_handler (void *args){
+	int newsockfd = (int*) args;
+	char buffer[256];
+	ssize_t n;
+	
+	/* If connection is established then start communicating */
+    while(!isClose){
+		bzero(buffer, sizeof(buffer));
+	    n = read(newsockfd, buffer, sizeof(buffer)); // recv on Windows
+	    if (n <= 0) {
+			perror("ERROR reading from socket");
+			deleteConnection(newsockfd);
+	    }
+	    printf("Here is the message: %s\n", buffer);
 
-    if (newsockfd < 0) {
-        perror("ERROR on accept");
-        exit(1);
+	    /* Write a response to the client */
+	    n = write(newsockfd, "I got your message", 18); // send on Windows
+
+	    if (n <= 0) {
+			perror("ERROR writing to socket");
+			deleteConnection(newsockfd);
+	    }
+	    bzero(buffer, 256);
+	    
     }
+	deleteConnection(newsockfd);
 
-    /* If connection is established then start communicating */
-    bzero(buffer, 256);
-    n = read(newsockfd, buffer, 255); // recv on Windows
 
-    if (n < 0) {
-        perror("ERROR reading from socket");
-        exit(1);
-    }
-
-    printf("Here is the message: %s\n", buffer);
-
-    /* Write a response to the client */
-    n = write(newsockfd, "I got your message", 18); // send on Windows
-
-    if (n < 0) {
-        perror("ERROR writing to socket");
-        exit(1);
-    }
-
-    return 0;
 }
