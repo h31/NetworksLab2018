c4eefe93bfcea3c606e3d796aac7a5d1517c89dc
Ivan Kylchik <cilcic.v22@gmail.com>
2018-10-15 22:11:04 +0300

    Реализовал простой UDP клиент/сервер

--- simple_udp/client/main.c
+++ simple_udp/client/main.c
@@ -4,14 +4,16 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include<arpa/inet.h>
 
 #include <string.h>
 
+void closeSocket(int sockfd, int error, char* errorMsg);
 int main(int argc, char *argv[]) {
     int sockfd, n;
     uint16_t portno;
     struct sockaddr_in serv_addr;
-    struct hostent *server;
+    unsigned int servlen = sizeof(serv_addr);
 
     char buffer[256];
 
@@ -23,29 +25,19 @@ int main(int argc, char *argv[]) {
     portno = (uint16_t) atoi(argv[2]);
 
     /* Create a socket point */
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
     if (sockfd < 0) {
         perror("ERROR opening socket");
         exit(1);
     }
 
-    server = gethostbyname(argv[1]);
-
-    if (server == NULL) {
-        fprintf(stderr, "ERROR, no such host\n");
-        exit(0);
-    }
-
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy(server->h_addr, (char *) &serv_addr.sin_addr.s_addr, (size_t) server->h_length);
     serv_addr.sin_port = htons(portno);
-
-    /* Now connect to the server */
-    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        perror("ERROR connecting");
-        exit(1);
+     
+    if (inet_aton(argv[1], &serv_addr.sin_addr) == 0){
+        closeSocket(sockfd, 1, "ERROR connecting");
     }
 
     /* Now ask for a message from the user, this message
@@ -57,22 +49,31 @@ int main(int argc, char *argv[]) {
     fgets(buffer, 255, stdin);
 
     /* Send message to the server */
-    n = write(sockfd, buffer, strlen(buffer));
+    n = sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *) &serv_addr, servlen);
 
     if (n < 0) {
-        perror("ERROR writing to socket");
-        exit(1);
+        closeSocket(sockfd, 1, "ERROR writing to socket");
     }
 
     /* Now read server response */
     bzero(buffer, 256);
-    n = read(sockfd, buffer, 255);
+    n = recvfrom(sockfd, buffer, 255, 0, (struct sockaddr *) &serv_addr, &servlen);
 
     if (n < 0) {
-        perror("ERROR reading from socket");
-        exit(1);
+        closeSocket(sockfd, 1, "ERROR reading from socket");
     }
 
     printf("%s\n", buffer);
+
+    close(sockfd);
     return 0;
+}
+
+void closeSocket(int sockfd, int error, char* errorMsg){
+    if(strcmp(errorMsg,"") != 0){
+        perror(errorMsg);
+    }
+    shutdown(sockfd, SHUT_RDWR);
+    close(sockfd);
+    exit(error);
 }
--- simple_udp/server/main.c
+++ simple_udp/server/main.c
@@ -7,16 +7,17 @@
 
 #include <string.h>
 
+void closeSocket(int sockfd, int error, char* errorMsg);
 int main(int argc, char *argv[]) {
-    int sockfd, newsockfd;
-    uint16_t portno;
-    unsigned int clilen;
+    int sockfd;
+    uint16_t portno = 5001;
     char buffer[256];
     struct sockaddr_in serv_addr, cli_addr;
+    unsigned int clilen = sizeof(cli_addr);
     ssize_t n;
 
     /* First call to socket() function */
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
     if (sockfd < 0) {
         perror("ERROR opening socket");
@@ -25,51 +26,48 @@ int main(int argc, char *argv[]) {
 
     /* Initialize socket structure */
     bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = 5001;
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
 
+    if(setsockopt(sockfd, SOL_SOCKET,(SO_REUSEPORT | SO_REUSEADDR),&(int){ 1 }, sizeof(int)) < 0){
+        closeSocket(sockfd, 1, "ERROR on setsockopt");
+    }
+
     /* Now bind the host address using bind() call.*/
     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-        perror("ERROR on binding");
-        exit(1);
+        closeSocket(sockfd, 1, "ERROR on binding");
     }
 
-    /* Now start listening for the clients, here process will
-       * go in sleep mode and will wait for the incoming connection
-    */
-
-    listen(sockfd, 5);
-    clilen = sizeof(cli_addr);
-
-    /* Accept actual connection from the client */
-    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-
-    if (newsockfd < 0) {
-        perror("ERROR on accept");
-        exit(1);
+    if(fork() > 0){
+        while(getchar() != 'q'){
+        }
+        closeSocket(sockfd, 0, "");
     }
 
-    /* If connection is established then start communicating */
-    bzero(buffer, 256);
-    n = read(newsockfd, buffer, 255); // recv on Windows
+    while (1) {
+        bzero(buffer, 256);
+        n = recvfrom(sockfd, buffer, 256, 0, (struct sockaddr *) &cli_addr, &clilen);
+        if (n < 0) {
+            closeSocket(sockfd, 1, "ERROR reading from socket");
+        }
+        printf("Here is the message: %s\n", buffer);
 
-    if (n < 0) {
-        perror("ERROR reading from socket");
-        exit(1);
-    }
-
-    printf("Here is the message: %s\n", buffer);
-
-    /* Write a response to the client */
-    n = write(newsockfd, "I got your message", 18); // send on Windows
-
-    if (n < 0) {
-        perror("ERROR writing to socket");
-        exit(1);
+        n = sendto(sockfd, "I got your message", 18, 0, (struct sockaddr *) &cli_addr, clilen);
+        if (n < 0) {
+            closeSocket(sockfd, 1, "ERROR writing to socket");
+        }
     }
 
     return 0;
+}
+
+void closeSocket(int sockfd, int error, char* errorMsg){
+    if(strcmp(errorMsg,"") != 0){
+        perror(errorMsg);
+    }
+    shutdown(sockfd, SHUT_RDWR);
+    close(sockfd);
+    exit(error);
 }
