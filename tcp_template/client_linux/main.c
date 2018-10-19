21d48966e478310ddf92436ee78af73f7574380f
emil151997 <emil151997@Mail.ru>
2018-10-19 05:29:00 +0300

    TCP Linux fixes

--- tcp_template/client_linux/main.c
+++ tcp_template/client_linux/main.c
@@ -14,7 +14,8 @@ int main(int argc, char *argv[]) {
 
     char buffer[256];
 
-    if (argc < 3) {
+    if (argc < 3)
+	{
         fprintf(stderr, "usage %s hostname port\n", argv[0]);
         exit(0);
     }
@@ -24,14 +25,16 @@ int main(int argc, char *argv[]) {
     /* Create a socket point */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (sockfd < 0) {
+    if (sockfd < 0)
+	{
         perror("ERROR opening socket");
         exit(1);
     }
 
     server = gethostbyname(argv[1]);
 
-    if (server == NULL) {
+    if (server == NULL)
+	{
         fprintf(stderr, "ERROR, no such host\n");
         exit(0);
     }
@@ -42,7 +45,8 @@ int main(int argc, char *argv[]) {
     serv_addr.sin_port = htons(portno);
 
     /* Now connect to the server */
-    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+	{
         perror("ERROR connecting");
         exit(1);
     }
@@ -50,13 +54,13 @@ int main(int argc, char *argv[]) {
     /* Now ask for a message from the user, this message
        * will be read by server
     */
-	
+
 	while(1)
     {
    	 	printf("Please enter the message: ");
     	bzero(buffer, 256);
 		fgets(buffer, 255, stdin);
-        
+
         /* Send message to the server */
         if( send(sockfd , buffer , 256 , 0) < 0)
         {
@@ -69,11 +73,9 @@ int main(int argc, char *argv[]) {
             puts("ERROR recv");
             break;
         }
-         
+
         }
-    
 		shutdown(sockfd, SHUT_RDWR);
     	close(sockfd);
     	return 0;
 }
-    
