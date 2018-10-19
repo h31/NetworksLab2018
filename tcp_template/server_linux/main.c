936c38f90fec0974113274a61d011043b0113da7
VakhaevIlya <shava223@mail.ru>
2018-10-19 06:48:13 +0300

    Fixed TCP Linux

--- tcp_template/server_linux/main.c
+++ tcp_template/server_linux/main.c
@@ -18,7 +18,8 @@ int main(int argc, char *argv[]) {
     /* First call to socket() function */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (sockfd < 0) {
+    if (sockfd < 0) 
+    {
         perror("ERROR opening socket");
         exit(1);
     }
@@ -32,8 +33,10 @@ int main(int argc, char *argv[]) {
     serv_addr.sin_port = htons(portno);
 
     /* Now bind the host address using bind() call.*/
-    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+    {
         perror("ERROR on binding");
+	close(sockfd);
         exit(1);
     }
 
@@ -43,24 +46,26 @@ int main(int argc, char *argv[]) {
 
     listen(sockfd, 5);
     clilen = sizeof(cli_addr);
-	pthread_t thread_num;
+    pthread_t thread_num;
 
     /* Accept actual connection from the client */
     while (newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen))
-	{
+    {
 		if( pthread_create( &thread_num , NULL , connection_handler , (void*) &newsockfd) < 0)
 		{
-            perror("could not create thread");
-            return 1;
-        }
+            		perror("could not create thread");
+			close(newsockfd);
+            		return 1;
+        	}
     }
-	if (newsockfd < 0) {
-        perror("ERROR on accept");
-        exit(1);
+	if (newsockfd < 0) 
+	{
+        	perror("ERROR on accept");
+		close(sockfd);
+        	exit(1);
 
-
-    return 0;
-}
+    		return 0;
+	}
 }
 void *connection_handler(void *sockfd)
 {
@@ -75,43 +80,52 @@ void *connection_handler(void *sockfd)
     {
 
 
-    if (r_size < 0) 
+    	if (r_size < 0) 
 	{
-        perror("ERROR reading from socket");
-        exit(1);
-    }
+        	perror("ERROR reading from socket");
+		close(sockt);
+		pthread_exit(NULL);
+        	exit(1);
+    	}
 
-    printf("Here is the message: %s\n", buffer);
+    	printf("Here is the message: %s\n", buffer);
 
-    /* Write a response to the client */
+    	/* Write a response to the client */
+	
 
-		
         //end of string marker
-		buffer[r_size] = '\0';
+	buffer[r_size] = '\0';
 		
-		//Send the message back to client
-		n=write(sockt, "I got your message", 18); // send on Windows
+	//Send the message back to client
+	n=write(sockt, "I got your message", 18); // send on Windows
 
-    	if (n < 0) 
-		{
+    	if (n <= 0) 
+	{
         	perror("ERROR writing to socket");
+		close(sockt);
+		pthread_exit(NULL);
         	exit(1);
     	}
 		
-		//clear the message buffer
-		memset(buffer, 0, 256);
+	//clear the message buffer
+	memset(buffer, 0, 256);
+    shutdown(sockt, SHUT_RDWR);
+	close(sockt);
+	pthread_exit(NULL);
     }
      	if(r_size == 0)
-	 	{	
+	{	
         	puts("Client disconnect ");
         	fflush(stdout);
     	}
     	else if(r_size == -1)
     	{
         	perror("Fail recv");
-   		 }
+   	}
+
     	free(sockfd);
+    shutdown(sockt, SHUT_RDWR);
     	close(sockt);
-		pthread_exit(NULL);
+	pthread_exit(NULL);
     	return 0;
 } 
