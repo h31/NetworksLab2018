0b889c219b01fdcce5f2f094e31630807d7e42c4
emil151997 <emil151997@mail.ru>
2018-10-20 06:34:23 +0300

    Simple UDP implementation

--- simple_udp/client_linux/main.c
+++ simple_udp/client_linux/main.c
@@ -7,7 +7,7 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    int sockfd, n;
+    int sockfd, clilen;
     uint16_t portno;
     struct sockaddr_in serv_addr;
     struct hostent *server;
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]) {
     portno = (uint16_t) atoi(argv[2]);
 
     /* Create a socket point */
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
     if (sockfd < 0)
 	{
@@ -41,19 +41,11 @@ int main(int argc, char *argv[]) {
 
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy(server->h_addr, (char *) &serv_addr.sin_addr.s_addr, (size_t) server->h_length);
     serv_addr.sin_port = htons(portno);
 
-    /* Now connect to the server */
-    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-	{
-        perror("ERROR connecting");
-        exit(1);
-    }
-
-    /* Now ask for a message from the user, this message
-       * will be read by server
-    */
+	
+	clilen = sizeof(serv_addr);
+   
 
 	while(1)
     {
@@ -62,13 +54,14 @@ int main(int argc, char *argv[]) {
 		fgets(buffer, 255, stdin);
 
         /* Send message to the server */
-        if( send(sockfd , buffer , 256 , 0) < 0)
+        if( sendto(sockfd , buffer , 256 , 0,(struct sockaddr *) &serv_addr, clilen) < 0)
         {
             puts("ERROR sending");
             return 1;
         }
+		memset(buffer,'\0', 256);
 		/* Now read server response */
-		if( recv(sockfd , buffer , 256 , 0) < 0)
+		if( recvfrom(sockfd , buffer , 256 , 0,(struct sockaddr *) &serv_addr, &clilen) < 0)
         {
             puts("ERROR recv");
             break;
--- simple_udp/server_linux/main.c
+++ simple_udp/server_linux/main.c
@@ -7,17 +7,16 @@
 #include <pthread.h>
 #include <string.h>
 
-void *connection_handler(void *);
 
 int main(int argc, char *argv[]) {
-    int sockfd, newsockfd,*new_socket;
+    int sockfd,received_size;
     uint16_t portno;
     unsigned int clilen;
     char buffer[256];
     struct sockaddr_in serv_addr, cli_addr;
    
     /* First call to socket() function */
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
     if (sockfd < 0) {
         perror("ERROR opening socket");
@@ -38,64 +37,30 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    /* Now start listening for the clients, here process will
-       * go in sleep mode and will wait for the incoming connection
-    */
 
-    listen(sockfd, 5);
     clilen = sizeof(cli_addr);
-
-    /* Accept actual connection from the client */
 	
-    while ((newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen)) )
+    while (1)
 	{
-		pthread_t new_thread;
-		new_socket = malloc(sizeof *new_socket);
-		*new_socket = newsockfd;
-		if( pthread_create( &new_thread , NULL ,  connection_handler , (void*) new_socket) < 0)
-        {
-            perror("ERROR creating thread");
-            return 1;
-        }
-	}
-	
-    if (newsockfd < 0) {
-        perror("ERROR on accept");
-        exit(1);
-    }
-
-    return 0;
-}
-void *connection_handler(void *sockfd)
-{
-    //Get the socket descriptor
-    int sockt = *(int*)sockfd;
-    int received_size;
-    char buffer[256];
-
-    //Receive a message from client
-    while( (received_size = recv(sockt , buffer , 256 , 0)) > 0 )
-    {
+		while((received_size = recvfrom(sockfd , buffer , 256 , 0,(struct sockaddr *) &cli_addr, &clilen)) > 0 )
+		{
         //Send the message back to client
-        write(sockt , buffer , strlen(buffer));
-        printf("Here is the message: %s\n", buffer);
-		memset(buffer ,'\0', 256);
-    }
-
-    if(received_size == 0)
-    {
-        puts("Client has been disconnected");
-        fflush(stdout);
-    }
-    else if(received_size == -1)
-    {
-        perror("ERROR recv");
-    }
-
-    //Free and close socket, terminate the socket
-    free(sockfd);
-    close(sockt);
-    pthread_exit(0); 
-    return 0;
+        	sendto(sockfd , buffer , received_size,0,(struct sockaddr*) &cli_addr, clilen);
+        	printf("Here is the message: %s\n", buffer);
+			memset(buffer ,'\0', 256);
+			printf("r_size = %d",received_size);
+		}
+    	else if(received_size == -1)
+    	{
+       	 perror("ERROR recv");
+		}
+		free(sockfd)
+		close(sockfd);
+		return 0;
 }
+}
+
+  
+ 
+
 	
