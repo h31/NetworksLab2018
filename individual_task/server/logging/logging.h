8735a6e597f94adb7c617799f7879bc7606e02ce
Nikita Kuzmin <kobalt31@mail.ru>
2018-11-17 17:08:43 +0300

    change logger

--- individual_task/server/logging/logging.h
+++ individual_task/server/logging/logging.h
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <time.h>
 #include <sys/time.h>
+#include <stdarg.h>
 
-#define mylog(msg) internal_log(__func__, msg)
+#define mlog(msg) internal_log(__func__, msg)
+#define mlogf(msg, ...) internal_log_formatted(__func__, msg, __VA_ARGS__)
+
+extern int console_log;
 
 void internal_log(char const * caller_func, char* msg);
+
+void internal_log_formatted(char const * caller_func, const char* msg, ...);
