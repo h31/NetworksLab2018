9a9452f8bfabd30e397060c63df9ad4ccc95041c
staslovelas <chertpaderi@gmail.com>
2018-11-18 15:17:50 +0300

    Исправление проблем с форматированием

--- simple_tcp/client_windows/main.c
+++ simple_tcp/client_windows/main.c
@@ -19,18 +19,18 @@ int main(int argc, char *argv[]) {
     struct hostent *server;
 
     char* buffer = (char*)calloc(256, sizeof(char));
-	
-	/*Initialize  library wsock32.dll*/
-	WSADATA WsaData; 
-	int  WsaError;
-	WsaError = WSAStartup(0x0101, &WsaData);
-	
-	if (WsaError!=0) {
-		perror("ERROR on WSAStartup");
-		exit(1);
-	}
-	
-	if (argc < 3) {
+    
+    /*Initialize  library wsock32.dll*/
+    WSADATA WsaData; 
+    int  WsaError;
+    WsaError = WSAStartup(0x0101, &WsaData);
+    
+    if (WsaError!=0) {
+        perror("ERROR on WSAStartup");
+        exit(1);
+    }
+    
+    if (argc < 3) {
         fprintf(stderr, "usage %s hostname port\n", argv[0]);
         exit(0);
     }
@@ -49,21 +49,21 @@ int main(int argc, char *argv[]) {
 
     if (server == NULL) {
         fprintf(stderr, "ERROR, no such host\n");
-		closeSocket(sockfd);
+        closeSocket(sockfd);
 
         exit(0);
     }
-	
-	/* Initialize socket structure */
+    
+    /* Initialize socket structure */
     memset((char *)&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-	memmove((char *)&serv_addr.sin_addr.s_addr, server->h_addr, (size_t)server->h_length); 
+    memmove((char *)&serv_addr.sin_addr.s_addr, server->h_addr, (size_t)server->h_length); 
     serv_addr.sin_port = htons(portno);
 
     /* Now connect to the server */
     if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == SOCKET_ERROR) {
         perror("ERROR connecting");
-		closeSocket(sockfd);
+        closeSocket(sockfd);
 
         exit(1);
     }
@@ -73,28 +73,28 @@ int main(int argc, char *argv[]) {
     fgets(buffer, 255, stdin);
 
     /* Send message to the server */
-   writeMessage(sockfd, buffer);
-   free(buffer);
+    writeMessage(sockfd, buffer);
+    free(buffer);
 
     /* Now read server response */
     buffer = readMessage(sockfd);
 
-	closeSocket(sockfd);
-	free(buffer);
+    closeSocket(sockfd);
+    free(buffer);
 
     return 0;
 }
 
 void closeSocket(SOCKET sock) {
-	shutdown(sock, SHUT_RDWR);
-	closesocket(sock);
+    shutdown(sock, SHUT_RDWR);
+    closesocket(sock);
 }
 
 void writeMessage (SOCKET sock, char* buffer) {
 
     uint32_t messlen;
     char* bufForLen = (char*)calloc(4, sizeof(char));
-	int n;
+    int n;
 
     /* Send length of message to the server */
     messlen = strlen(buffer);
@@ -125,7 +125,7 @@ char* readMessage(SOCKET sock) {
     char* buffer = (char*)calloc(256, sizeof(char));
     char* bufForLen = (char*)calloc(4, sizeof(char));
     uint32_t messlen;
-	int n;
+    int n;
 
     /*Read lenght of message from the server*/
     n = recv(sock, bufForLen, 4, 0); 
--- simple_tcp/server_windows/main.c
+++ simple_tcp/server_windows/main.c
@@ -16,23 +16,23 @@ char* readMessage (SOCKET sock, SOCKET newsock);
 void writeMessage (SOCKET newsock, char* buffer);
 
 int main(int argc, char *argv[]) {
-	/*Initialize  library wsock32.dll*/
-	WSADATA WsaData; 
-	int  WsaError; 
-	
-	WsaError = WSAStartup(0x0101, &WsaData);
-	
-	if (WsaError!=0) {
-		perror("ERROR on WSAStartup");
-		exit(1);
-	}
-	
-	/* Create thread for waiting of request */
-	CreateThread(NULL, 0, waitFunc, NULL, 0, NULL);
-	while(getchar() != 'q') {
-	}
-	
-	return 0;
+    /*Initialize  library wsock32.dll*/
+    WSADATA WsaData; 
+    int  WsaError; 
+    
+    WsaError = WSAStartup(0x0101, &WsaData);
+    
+    if (WsaError!=0) {
+        perror("ERROR on WSAStartup");
+        exit(1);
+    }
+    
+    /* Create thread for waiting of request */
+    CreateThread(NULL, 0, waitFunc, NULL, 0, NULL);
+    while(getchar() != 'q') {
+    }
+    
+    return 0;
 }
 
 /* Function to create socket and wait for requests*/
@@ -40,8 +40,8 @@ DWORD WINAPI waitFunc() {
     SOCKET sockfd;
     uint64_t portno;
     struct sockaddr_in serv_addr;
-	BOOL temp;
-	
+    BOOL temp;
+    
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -57,107 +57,107 @@ DWORD WINAPI waitFunc() {
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
-	
-	temp = TRUE;
+    
+    temp = TRUE;
 
-	if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char *) &temp, sizeof(BOOL)) < 0) {
-	    	perror("ERROR on setsockopt");
-	    	closeSocket(sockfd);
-		exit(1);
+    if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char *) &temp, sizeof(BOOL)) < 0) {
+            perror("ERROR on setsockopt");
+            closeSocket(sockfd);
+        exit(1);
     }
 
     /* Now bind the host address using bind() call.*/
     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
         perror("ERROR on binding");
-		closeSocket(sockfd);
-		
+        closeSocket(sockfd);
+        
         exit(1);
     }
-	
+    
     /* Now start listening for the clients*/
     listen(sockfd, 5);
-    	while(1) {
-		if (flag == TRUE) {
-			flag = FALSE;
-			CreateThread(NULL, 0, commFunc, &sockfd, 0, NULL);
-		}
-	}
+        while(1) {
+            if (flag == TRUE) {
+                flag = FALSE;
+                CreateThread(NULL, 0, commFunc, &sockfd, 0, NULL);
+            }
+        }
 }
 
 /*Function for handling request*/
 DWORD WINAPI commFunc(LPVOID temp) {
     int clilen;
-	struct sockaddr_in cli_addr;
-	int sockfd;
+    struct sockaddr_in cli_addr;
+    int sockfd;
 
-	char* buffer = (char*)calloc(256, sizeof(char));
+    char* buffer = (char*)calloc(256, sizeof(char));
 
-	clilen = sizeof(cli_addr);
-	sockfd = *(int *) temp;
-	
-	 /* Accept actual connection from the client */
-	SOCKET newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-	flag = TRUE;
-	if (newsockfd == INVALID_SOCKET) {
-	    perror("ERROR on accept");
-		closeSocket(sockfd);
-		
-	    exit(1);
-	}
-	 /*Get the message from client*/
-	buffer = readMessage(sockfd, newsockfd);
-	printf("Here is the message: %s\n", buffer);	
-	free(buffer);
-	
-	 /*Write a response  to the client*/
-	writeMessage(newsockfd, "I GOT YOUR MESSAGE");
-	
-	closesocket(newsockfd);
-	return 0;
+    clilen = sizeof(cli_addr);
+    sockfd = *(int *) temp;
+    
+     /* Accept actual connection from the client */
+    SOCKET newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    flag = TRUE;
+    if (newsockfd == INVALID_SOCKET) {
+        perror("ERROR on accept");
+        closeSocket(sockfd);
+        
+        exit(1);
+    }
+     /*Get the message from client*/
+    buffer = readMessage(sockfd, newsockfd);
+    printf("Here is the message: %s\n", buffer);	
+    free(buffer);
+    
+     /*Write a response  to the client*/
+    writeMessage(newsockfd, "I GOT YOUR MESSAGE");
+    
+    closesocket(newsockfd);
+    return 0;
 } 
 
 void closeSocket(SOCKET sock) {
-	shutdown(sock, SHUT_RDWR);
-	closesocket(sock);
+    shutdown(sock, SHUT_RDWR);
+    closesocket(sock);
 }
 
 char* readMessage (SOCKET sock, SOCKET newsock) {
-	int n;
-	uint32_t messlen;
-	char* buffer = (char*)calloc(256, sizeof(char));
-	char* bufForLen = (char*)calloc(4, sizeof(char));
-	
-	n = recv(newsock, bufForLen, 4, 0); 
+    int n;
+    uint32_t messlen;
+    char* buffer = (char*)calloc(256, sizeof(char));
+    char* bufForLen = (char*)calloc(4, sizeof(char));
+    
+    n = recv(newsock, bufForLen, 4, 0); 
 
-	if (n < 0) {
-		perror("ERROR lenght of message");
-		closeSocket(sock);
-		closeSocket(newsock);
-		
-		exit(1);
-	}
-	
-	messlen = atol(bufForLen);
-	free(bufForLen);
+    if (n < 0) {
+        perror("ERROR lenght of message");
+        closeSocket(sock);
+        closeSocket(newsock);
+        
+        exit(1);
+    }
+    
+    messlen = atol(bufForLen);
+    free(bufForLen);
 
-	for(unsigned int i = 0; i < messlen; i += n) {
-		n = recv(newsock, buffer + i, 255, 0); 
-		if (n < 0) {
-			perror("ERROR of message");
-			closeSocket(sock);
-			closeSocket(newsock);
-			
-			exit(1);
-		}
-	}
-	return buffer;
+    for(unsigned int i = 0; i < messlen; i += n) {
+        n = recv(newsock, buffer + i, 255, 0); 
+        if (n < 0) {
+            perror("ERROR of message");
+            closeSocket(sock);
+            closeSocket(newsock);
+            
+            exit(1);
+        }
+    }
+    return buffer;
 }
 
 void writeMessage (SOCKET newsock, char* buffer) {
-	int n;
-	uint32_t messlen;
+    int n;
+    uint32_t messlen;
     char* bufForLen = (char*)calloc(4, sizeof(char));
-	
+    
     messlen = strlen(buffer);
     sprintf(bufForLen, "%04d", messlen);
     n = send(newsock, bufForLen, strlen(bufForLen), 0);
@@ -169,13 +169,13 @@ void writeMessage (SOCKET newsock, char* buffer) {
         exit(1);
     }
     free(bufForLen);
-	
-	n = send(newsock, buffer, messlen, 0); 
+    
+    n = send(newsock, buffer, messlen, 0); 
 
-	if (n < 0) {
-		perror("ERROR writing to socket");
-		closeSocket(newsock);
-		
-		exit(1);
-	}
+    if (n < 0) {
+        perror("ERROR writing to socket");
+        closeSocket(newsock);
+        
+        exit(1);
+    }
 }
