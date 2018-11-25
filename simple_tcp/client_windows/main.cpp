2004cf38904762c68f831d48cd083182fbc169c6
OlegShust <shus.oleg@gmail.com>
2018-11-25 14:18:30 +0300

    update main client_win

--- simple_tcp/client_windows/main.cpp
+++ simple_tcp/client_windows/main.cpp
@@ -1,67 +1,19 @@
-#define WIN32_LEAN_AND_MEAN
-
-#include <windows.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
 #pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+#pragma warning(disable:4996)
 
 #define DEFAULT_BUFLEN 512
 #define DEFAULT_PORTION 9
-#define DEFAULT_CLIENT_COUNT 4
 #define DEFAULT_PORT "27015"
-
-struct shared_data
-{
-	HANDLE mutex;
-	HANDLE client_threads[DEFAULT_CLIENT_COUNT];
-	int client_count;
-	SOCKET socket_array[DEFAULT_CLIENT_COUNT];
-	
-
-	shared_data()
-	{
-		client_count = 0;
-		mutex = NULL;
-		for (int i = 0; i < DEFAULT_CLIENT_COUNT; ++i)
-		{
-			client_threads[i] = NULL;
-			socket_array[i] = INVALID_SOCKET;
-		}
-	}
-};
-
-shared_data g_shared_data;
-
-void kill_client(int n)
-{
-	WaitForSingleObject(g_shared_data.mutex, INFINITE);
-	auto sock = g_shared_data.socket_array[n];
-	if (sock == INVALID_SOCKET ||
-		sock == SOCKET_ERROR) return;
-	printf("shutting down client socket #%d\n", n);
-	int iResult = shutdown(sock, SD_BOTH);
-	if (iResult == SOCKET_ERROR) {
-		printf("socket #%d still in use\n", n);
-	}
-	closesocket(sock);
-	g_shared_data.socket_array[n] = INVALID_SOCKET;
-	ReleaseMutex(g_shared_data.mutex);
-}
-
-bool startsWith(const char* str, const char* starts)
-{
-	if (str == 0 || starts == 0) return false;
-	int length = strlen(starts);
-	if (strlen(str) < length) return false;
-	for (int i = 0; i < length; ++i)
-	{
-		if (str[i] != starts[i]) return false;
-	}
-	return true;
-}
 
 int readn(SOCKET sock, char* buf, int buflen, int _n)
 {
@@ -80,244 +32,156 @@ int readn(SOCKET sock, char* buf, int buflen, int _n)
 			return SOCKET_ERROR;
 		}
 		else {
-			printf("recv failed with error: %d\n", WSAGetLastError());
+			printf("recv failed with error: %d\nEXITING\n", WSAGetLastError());
 			return SOCKET_ERROR;
 		}
 	}
 	return n;
 }
 
-
-DWORD WINAPI client_thread(void* data)
+int __cdecl main(int argc, char **argv)
 {
-	int number = *((int*)data);
-
-	int iResult, iSendResult;
+	WSADATA wsaData;
+	SOCKET ConnectSocket = INVALID_SOCKET;
+	struct addrinfo *result = NULL,
+		*ptr = NULL,
+		hints;
+	char sendbuf[DEFAULT_BUFLEN] = { 0 };
 	char recvbuf[DEFAULT_BUFLEN] = { 0 };
+	int iResult;
 	int recvbuflen = DEFAULT_BUFLEN;
 
-	// Accept a client socket
-	SOCKET sock = g_shared_data.socket_array[number];
-	
-	// No longer need server socket
-	printf("%d: connected to remote user\n", number);
-	printf("%d: receiving data...\n", number);
-	// Receive until the peer shuts down the connection
-	do {
-		ZeroMemory(recvbuf, sizeof(recvbuf));
-
-		iResult = readn(sock, recvbuf, DEFAULT_BUFLEN, DEFAULT_PORTION);
-		if (iResult > 0) {
-			printf("%d: Received:'%s' (%d bytes)\n", number, recvbuf, iResult);
-
-			// Echo the buffer back to the sender
-			iSendResult = send(sock, recvbuf, iResult, 0);
-			if (iSendResult == SOCKET_ERROR) {
-				printf("%d: send back failed with error: %d\nEXITING\n", number, WSAGetLastError());
-
-				break;
-			}
-			printf("%d: Sending back:'%s' (%d bytes)\n", number, recvbuf, iSendResult);
-		}
-		else if (iResult == SOCKET_ERROR)
-		{
-			break;
-		}
-
-	} while (iResult > 0);
-
-
-	if (sock != INVALID_SOCKET)
-	{
-		kill_client(number);
-	}
-
-
-	delete ((int*)data);
-
-	printf("%d: disconnected\n", number);
-	return 0;
-}
-
-DWORD WINAPI listen_thread(void* data)
-{
-	SOCKET server_sock = *((SOCKET*)data);
-
-	// Accept a client socket
-	int number = g_shared_data.client_count;
-	while (true)
-	{
-		SOCKET& sock = g_shared_data.socket_array[number];
-
-		sock = accept(server_sock, NULL, NULL);
-
-		if (sock == INVALID_SOCKET) {
-			printf("server socket failure, disconnecting clients\n");
-			break;
-		}
-
-		if (number < DEFAULT_CLIENT_COUNT)
-		{
-			int* params = new int;
-			*params = number;
-			WaitForSingleObject(g_shared_data.mutex, INFINITE);
-			g_shared_data.client_threads[number] = CreateThread(NULL, 0, client_thread, params, 0, NULL);
-			ReleaseMutex(g_shared_data.mutex);
-			++number;
-		}
-		else
-		{
-			printf("warning: Could not connect new client: out of slots.\n");
-		}
-	}
-
-	for (int i = 0; i < DEFAULT_CLIENT_COUNT; ++i)
-	{
-		kill_client(i);
-	}
-
-	printf("waiting for client threads to finish...\n");
-
-	WaitForMultipleObjects(DEFAULT_CLIENT_COUNT, g_shared_data.client_threads, TRUE, INFINITE);
-
-	delete ((SOCKET*)data);
-
-	return 0;
-}
-
-int main(void)
-{
-	HANDLE lthread;
-	WSADATA wsaData;
-
-	g_shared_data.mutex = CreateMutex(NULL, FALSE, NULL);
-	if (g_shared_data.mutex == NULL)
-	{
-		printf("failed to create mutex\n");
-		return 1;
-	}
-
-	struct addrinfo *result = NULL;
-	struct addrinfo hints;
-	int iResult;
+	const char* server_name = "localhost";
 
 	// Initialize Winsock
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (iResult != 0) {
 		printf("WSAStartup failed with error: %d\n", iResult);
+		getchar();
 		return 1;
 	}
 
 	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_INET;
+	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_protocol = IPPROTO_TCP;
-	hints.ai_flags = AI_PASSIVE;
 
 	// Resolve the server address and port
-	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+	iResult = getaddrinfo(server_name, DEFAULT_PORT, &hints, &result);
 	if (iResult != 0) {
 		printf("getaddrinfo failed with error: %d\n", iResult);
 		WSACleanup();
+		getchar();
 		return 1;
 	}
 
-	// Create a SOCKET for connecting to server
-	SOCKET server_sock = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-	if (server_sock == INVALID_SOCKET) {
-		printf("server socket failed with error: %ld\n", WSAGetLastError());
-		freeaddrinfo(result);
-		WSACleanup();
-		return 1;
-	}
+	printf("connecting to %s:%s...\n", server_name, DEFAULT_PORT);
+	// Attempt to connect to an address until one succeeds
+	for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
 
-	// Setup the TCP listening socket
-	printf("listening for port %s...\n", DEFAULT_PORT);
-	iResult = bind(server_sock, result->ai_addr, (int)result->ai_addrlen);
-	if (iResult == SOCKET_ERROR) {
-		printf("bind failed with error: %d\n", WSAGetLastError());
-		freeaddrinfo(result);
-		closesocket(server_sock);
-		server_sock = INVALID_SOCKET;
-		WSACleanup();
-		return 1;
+		// Create a SOCKET for connecting to server
+		ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
+			ptr->ai_protocol);
+		if (ConnectSocket == INVALID_SOCKET) {
+			printf("socket failed with error: %ld\n", WSAGetLastError());
+			WSACleanup();
+			getchar();
+			return 1;
+		}
+
+		// Connect to server.
+		iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+		if (iResult == SOCKET_ERROR) {
+			closesocket(ConnectSocket);
+			ConnectSocket = INVALID_SOCKET;
+			continue;
+		}
+		break;
 	}
 
 	freeaddrinfo(result);
 
-	iResult = listen(server_sock, SOMAXCONN);
-	if (iResult == SOCKET_ERROR) {
-		printf("listen failed with error: %d\n", WSAGetLastError());
-		closesocket(server_sock);
-		server_sock = INVALID_SOCKET;
+	if (ConnectSocket == INVALID_SOCKET) {
+		printf("Unable to connect to server!\n");
 		WSACleanup();
+		getchar();
 		return 1;
 	}
 
-	SOCKET* params = new SOCKET;
-	*params = server_sock;
-	lthread = CreateThread(NULL, 0, listen_thread, params, 0, NULL);
+	int n_total = 0;
+	int n_expected = 0;
 
-	char cmdbuf[DEFAULT_BUFLEN] = { 0 };
-
+	printf("connected to %s:%s...\n", server_name, DEFAULT_PORT);
 	while (true)
 	{
-		gets_s(cmdbuf, DEFAULT_BUFLEN);
-
-		if (strcmp(cmdbuf, "exit") == 0)
+		gets_s(sendbuf, DEFAULT_BUFLEN);
+		if (strlen(sendbuf) != 0)
 		{
-			break;
-		}
-		else if (strlen(cmdbuf) == 0)
-		{
-			;
-		}
-		else if (strcmp(cmdbuf, "list") == 0)
-		{
-			printf("connection list:\n");
-			WaitForSingleObject(g_shared_data.mutex, INFINITE);
-			for (int i = 0; i < DEFAULT_CLIENT_COUNT; ++i)
-			{
-				auto sock = g_shared_data.socket_array[i];
-				if (sock != INVALID_SOCKET &&
-					sock != SOCKET_ERROR)
-				{
-					printf("%d: remote user connected on socket %d\n", i, sock);
-				}
+			iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
+			if (iResult == SOCKET_ERROR) {
+				printf("send failed with error: %d\n", WSAGetLastError());
+				closesocket(ConnectSocket);
+				WSACleanup();
+				getchar();
+				return 1;
 			}
-			ReleaseMutex(g_shared_data.mutex);
+
+			printf("Bytes Sent: %ld\n", iResult);
+
+			ZeroMemory(recvbuf, sizeof(recvbuf));
+			
+			n_total += iResult;
+			while (n_total >= DEFAULT_PORTION)
+			{
+				n_total -= DEFAULT_PORTION;
+				n_expected += DEFAULT_PORTION;
+			}
+
+			while (n_expected > 0)
+			{
+				iResult = readn(ConnectSocket, recvbuf, DEFAULT_BUFLEN, DEFAULT_PORTION);
+				if (iResult == SOCKET_ERROR) {
+					printf("readn failed with error: %d\nEXITING\n", WSAGetLastError());
+					closesocket(ConnectSocket);
+					WSACleanup();
+					getchar();
+					return 1;
+				}
+				printf("Received:'%s' (%d bytes)\n", recvbuf, iResult);
+				n_expected -= 9;
+			}
+			
 		}
-		else if (startsWith(cmdbuf, "kill"))
-		{
-			char num[16] = { 0 };
-			strcpy_s(num, 15, cmdbuf + 4);
-			if (strlen(num) == 0) return false;
-			int n = atoi(num);
-			kill_client(n);
-		}
-		else
-		{
-			printf("Unrecognized command. Use 'exit' to exit, 'kill 0..%d' to disconnect client, 'list' to list all clients\n", DEFAULT_CLIENT_COUNT - 1);
-		}
+		else break;
 	}
+	
 
-
-	printf("shutting down server socket...\n");
-
-	// shutdown the connection since we're done
-	iResult = shutdown(server_sock, SD_BOTH);
+	// shutdown the connection since no more data will be sent
+	iResult = shutdown(ConnectSocket, SD_SEND);
 	if (iResult == SOCKET_ERROR) {
-		printf("server socket still in use\n");
+		printf("shutdown failed with error: %d\n", WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		getchar();
+		return 1;
 	}
-	closesocket(server_sock);
 
-	WaitForSingleObject(lthread, INFINITE);
+	// Receive until the peer closes the connection
+	do {
+		ZeroMemory(recvbuf, sizeof(recvbuf));
+		iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+		if (iResult > 0)
+			printf("Bytes received: %d\n", iResult);
+		else if (iResult == 0)
+			printf("connection closed\n");
+		else
+			printf("recv failed with error: %d\n", WSAGetLastError());
 
-	CloseHandle(g_shared_data.mutex);
+	} while (iResult > 0);
 
+	// cleanup
+	closesocket(ConnectSocket);
 	WSACleanup();
-
-	printf("main thread finished\n");
 
 	getchar();
 	return 0;
