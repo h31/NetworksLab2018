e400cc38cfa31cffd730d26d5dab3d9d4e3b6f42
OlegShust <shus.oleg@gmail.com>
2018-11-28 13:30:41 +0300

    fix bug tcp client

--- tcp_template/client_linux/main.cpp
+++ tcp_template/client_linux/main.cpp
@@ -4,6 +4,9 @@
 
 int main(int argc, char* argv[]) 
 {
+	// disable buffering
+	setbuf(stdout, NULL);
+
 	Server server;
 	Address master_address;
 	master_address.hostname = "127.0.0.1";
@@ -17,9 +20,13 @@ int main(int argc, char* argv[])
 		std::cin.getline(buffer, Message_size_limit);
 		if (buffer[0] == 0) break;
 
+		printf("Now sending: %s\n", buffer);
+
 		server.send(&master_address, std::string(buffer));
 
 		if (!server.running()) break;
+
+		printf("Awaiting response from server...\n");
 
 		Message message;
 		server.read_server(message);
