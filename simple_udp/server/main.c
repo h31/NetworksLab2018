8403a440afbe66b5110ad91158dbc7af95975581
Pox3R <rombl497@gmail.com>
2018-12-02 23:27:25 +0300

    Code formatting
    Code formatting

--- simple_udp/server/main.c
+++ simple_udp/server/main.c
@@ -18,8 +18,7 @@ int sendD;
 int rec;
 char buffer[256];
 
-void clear_all()
-{
+void clear_all() {
 	isClose = CLOSED;
 	char gotMsg[46] = "Server closed. Don't send anything, please";
 	int sendi = sendto(sockfd, gotMsg, rec, 0, (struct sockaddr*) &cli_addr, clilen);
@@ -28,8 +27,8 @@ void clear_all()
 }
 
 int main(int argc, char *argv[]) {
-	//CTRL+C
-	signal(SIGINT, clear_all);
+    //CTRL+C
+    signal(SIGINT, clear_all);
 
     /* First call to socket() function */
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -47,33 +46,28 @@ int main(int argc, char *argv[]) {
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
 	
-	n = bind(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr));
+    n = bind(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr));
 	
-	if (n < 0) 
-	{
-		perror("error on bind");
-		exit(1);
-	}
+    if (n < 0) {
+	perror("error on bind");
+	exit(1);
+    }
 	
-	clilen = sizeof(cli_addr);
+    clilen = sizeof(cli_addr);
 	
-	while(!isClose)
-	{
-		while ((rec = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *) &cli_addr, &clilen)) > 0)
-		{
-			char gotMsg[21] = "I've got your message";
-			sendD = sendto(sockfd, gotMsg, rec, 0, (struct sockaddr*) &cli_addr, clilen);
-			if (sendD < 0)
-			{
-				perror("error on send");
-				exit(1);
-			}
-			printf("Here is the message: %s\n", buffer);
-			bzero(buffer, sizeof(buffer));
-
+    while(!isClose) {
+	    while ((rec = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *) &cli_addr, &clilen)) > 0) {
+		char gotMsg[21] = "I've got your message";
+		sendD = sendto(sockfd, gotMsg, rec, 0, (struct sockaddr*) &cli_addr, clilen);
+		if (sendD < 0) {
+			perror("error on send");
+			exit(1);
 		}
-	}
-	printf("\n Closing server...\r\n");
+		printf("Here is the message: %s\n", buffer);
+		bzero(buffer, sizeof(buffer));
+	    }
+    }
+    printf("\n Closing server...\r\n");
     clear_all();
     return 0;
 }
