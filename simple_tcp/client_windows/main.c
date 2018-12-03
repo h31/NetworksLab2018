c590872f81a54f9e6c55673d36229ffad7bb0e55
Eldar Goloviznin <radleweird@gmail.com>
2018-12-03 10:27:23 +0300

    started tcp_windows

---
+++ simple_tcp/client_windows/main.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#include <string.h>
+
+#include <signal.h>
+
+//#define IMITATE_PARTLY_SENDING
+
+int sockfd;
+
+void closeApp() {
+	printf("Closing socket\r\n");
+	shutdown(sockfd, SHUT_RDWR);
+	close(sockfd);
+	exit(0);
+}
+
+int main(int argc, char *argv[]) {
+	signal(SIGINT, closeApp);
+	signal(SIGTERM, closeApp)
+	
+	int n;
+	uint16_t portno;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
+
+	char buffer[256];
+
+	if (argc < 3) {
+		fprintf(stderr, "usage %s hostname port\n", argv[0]);
+		exit(0);
+	}
+
+	portno = (uint16_t) atoi(argv[2]);
+
+	/* Create a socket point */
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (sockfd < 0) {
+		perror("ERROR opening socket");
+		exit(1);
+	}
+
+	server = gethostbyname(argv[1]);
+
+	if (server == NULL) {
+		fprintf(stderr, "ERROR, no such host\n");
+		exit(0);
+	}
+
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy(server->h_addr, (char *) &serv_addr.sin_addr.s_addr, (size_t) server->h_length);
+	serv_addr.sin_port = htons(portno);
+
+	/* Now connect to the server */
+	if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+		perror("ERROR connecting");
+		exit(1);
+	}
+
+	/* Now ask for a message from the user, this message
+	* will be read by server
+	*/
+	
+	int textInputSize = 0;
+	
+	while (1) {
+		printf("Please enter the message: ");
+		bzero(buffer, sizeof(buffer));
+		fgets(buffer + 1, sizeof(buffer) - 2, stdin);
+		
+		if (strstr(buffer + 1, "\\q") != NULL) {
+			closeApp();
+		}
+			
+		textInputSize = strlen(buffer + 1);
+		printf("Message size = %d\r\n", textInputSize);
+		buffer[0] = textInputSize;
+		
+		/* Send message to the server */
+		#ifdef IMITATE_PARTLY_SENDING
+			int firstPartSize = 4;
+			n = write(sockfd, buffer, firstPartSize);
+			sleep(2);
+			n = write(sockfd, buffer + firstPartSize, textInputSize + 2 - firstPartSize);
+		#else
+			n = write(sockfd, buffer, textInputSize + 2);
+		#endif
+
+		if (n < 0) {
+			perror("ERROR writing to socket");
+			closeApp();
+		}
+
+		/* Now read server response */
+		bzero(buffer, sizeof(buffer));
+		n = read(sockfd, buffer, sizeof(buffer) - 1);
+
+		if (n <= 0) {
+			perror("ERROR reading from socket");
+			closeApp();
+		}
+		
+		int responseLength = buffer[0];
+		
+		while (responseLength > n) {
+			int addedBytesCount = read(sockfd, buffer + n, sizeof(buffer) - 1 - n);
+			
+			if (addedBytesCount <= 0) {
+				perror("ERROR reading from socket");
+				closeApp();
+			}
+			
+			n += addedBytesCount;
+		}
+
+		printf("%s\n", buffer + 1);
+	}
+}
---
+++ simple_tcp/server_windows/main.c
@@ -0,0 +1,229 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <windows.h>
+#include <winsock2.h>
+
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int sockfd;
+
+int const MAX_SECONDS_READ_TIMEOUT = 5;
+
+int const MAX_CLIENT_COUNT = 10;
+int totalActiveClientsCount = 0;
+int isFinishing = FALSE;
+
+pthread_mutex_t mutexTotalAcitveClientsCount;
+
+void closeSocket(int socket) {
+	printf("Closing socket = %d \r\n", socket);
+	shutdown(socket, SHUT_RDWR);
+	close(socket);
+}
+
+void cleanUpClientConnectionThread(int newsockfd) {
+	closeSocket(newsockfd);
+	
+	pthread_mutex_lock(&mutexTotalAcitveClientsCount);
+	printf("Handler starts deactivating for newsockfd = %d\r\n", newsockfd);
+	totalActiveClientsCount--;
+	printf("Total active clients = %d\r\n", totalActiveClientsCount);
+	pthread_mutex_unlock(&mutexTotalAcitveClientsCount);
+
+	pthread_exit(0);
+}
+
+void handleClientConnection(void* arg) {
+	int newsockfd = (int*) arg;
+	
+	struct timeval timeout;
+	timeout.tv_sec = MAX_SECONDS_READ_TIMEOUT;
+	timeout.tv_usec = 0;
+	// On timeout read returns -1
+	setsockopt(newsockfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout));
+	
+	char buffer[256];
+	ssize_t totalBytesCount;
+	ssize_t additionalBytesCount;
+	ssize_t messageBytesLength;
+	
+	messageBytesLength = 0;
+	
+	while (!isFinishing) {
+		// Start reading new message
+		bzero(buffer, sizeof(buffer));
+		
+		totalBytesCount = read(newsockfd, buffer, sizeof(buffer) - 1);
+		
+		if (totalBytesCount == -1) {
+			continue;
+		} else if (totalBytesCount <= 0) {
+			cleanUpClientConnectionThread(newsockfd);
+		}
+		
+		messageBytesLength = buffer[0];
+		
+		printf("Initially read [%ld] bytes of [%ld] messageBytesLength\r\n", totalBytesCount, messageBytesLength);
+		
+		while (messageBytesLength > totalBytesCount) {
+			// Continue reading message to assemble full size message
+			printf("Reading additional bytes from = %d\r\n", newsockfd);
+			if (isFinishing) {
+				cleanUpClientConnectionThread(newsockfd);
+			}
+			
+			additionalBytesCount = read(newsockfd, buffer + totalBytesCount, sizeof(buffer) - 1 - totalBytesCount);
+			
+			if (additionalBytesCount == -1) {
+				continue;
+			} else if (additionalBytesCount <= 0) {
+				cleanUpClientConnectionThread(newsockfd);
+			}
+
+			totalBytesCount += additionalBytesCount;
+			
+			printf("Read [%ld] bytes of [%ld] messageBytesLength\r\n", totalBytesCount, messageBytesLength);
+		}
+		
+		printf("Read from = %d:\r\n%s", newsockfd, buffer + 1);
+
+		/* Write a response to the client */
+		bzero(buffer, sizeof(buffer));
+		sprintf(buffer + 1, "I got your message");
+		buffer[0] = 18;
+		totalBytesCount = write(newsockfd, buffer, buffer[0] + 1);
+
+		if (totalBytesCount < 0) {
+			printf("ERROR ON SENDING BACK, DISCONNECTING\r\n");
+			cleanUpClientConnectionThread(newsockfd);
+		}
+	}
+	
+	// Server is finishing -> cleaning up
+	cleanUpClientConnectionThread(newsockfd);
+}
+
+void acceptNewClients() {
+	printf("Ready for accepting incomming connections, sockfd = %d\r\n", sockfd);
+
+	int newsockfd;
+	unsigned int clilen;
+	struct sockaddr_in cli_addr;
+
+	listen(sockfd, 5);
+	clilen = sizeof(cli_addr);
+
+	pthread_t nextClientThread;
+	int nextClientThreadCreationResult;
+	
+	while (!isFinishing) {
+		if (totalActiveClientsCount == MAX_CLIENT_COUNT) {
+			// Limit for maximum clients count, the printf below can produce infinite flood
+			// printf("Connection limit reached, new user will hanging on connect\r\n");
+			continue;
+		}
+		
+		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+		if (newsockfd < 0) {
+			printf("ERROR ON ACCEPT, SKIPPING\r\n");
+			continue;
+		}
+		
+		nextClientThreadCreationResult = pthread_create(&nextClientThread, NULL, handleClientConnection, (void*) newsockfd);
+		if (nextClientThreadCreationResult != 0) {
+			printf("Error on creating client thread, error number: %d\r\n", nextClientThreadCreationResult);
+		} else {
+			pthread_detach(nextClientThread);
+			pthread_mutex_lock(&mutexTotalAcitveClientsCount);
+			totalActiveClientsCount++;
+			printf("Creating handler for newsockfd = %d\r\n", newsockfd);
+			printf("Total active clients = %d\r\n", totalActiveClientsCount);
+			pthread_mutex_unlock(&mutexTotalAcitveClientsCount);
+		}
+	}
+	
+	pthread_exit(0);
+}
+
+void mainThreadCleanUp() {
+	isFinishing = TRUE;
+	
+	printf("Started cleaning up \r\n");
+	
+	while (totalActiveClientsCount != 0) {
+		// Wait...
+		printf("There are some clients on the end, count = %d\r\n", totalActiveClientsCount);
+		sleep(1);
+	}
+	
+	printf("Deactivating server socket \r\n");
+	closeSocket(sockfd);
+	
+	printf("Program finished\r\n");
+	
+	exit(0);
+}
+
+int main() {
+	// When CTRL+C pressed within terminal -> cleaning up
+	signal(SIGINT, mainThreadCleanUp);
+	
+	// Program is about to terminate
+	signal(SIGTERM, mainThreadCleanUp);
+	
+	struct sockaddr_in serv_addr;
+	uint16_t portno;
+
+	/* First call to socket() function */
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (sockfd < 0) {
+		perror("ERROR opening socket");
+		exit(1);
+	}
+
+	/* Initialize socket structure */
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	portno = 5001;
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+
+	/* Now bind the host address using bind() call.*/
+	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+		perror("ERROR on binding");
+		exit(1);
+	}
+	
+	// Creating second thread for handling incoming connections
+	pthread_t incomingConnectionsWorkerThread;
+	int workerThreadCreationResult;
+	workerThreadCreationResult = pthread_create(&incomingConnectionsWorkerThread, NULL, acceptNewClients, NULL);
+	if (workerThreadCreationResult != 0) {
+		printf("Error on creating worker thread, error number: %d\r\n", workerThreadCreationResult);
+	}
+	
+	// Reading server-side commands from terminal
+	char command; 
+	
+	while (TRUE) {
+		command = getchar();
+		if (command == 'q') {
+			break;
+		} else {
+			// Do some stuff
+		}
+	}
+	
+	// Perfect moment for cleaning up
+	mainThreadCleanUp();
+	
+	return 0;
+}
