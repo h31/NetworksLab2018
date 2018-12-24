49de38b0832c15fd4d5b81b8ab25baf5c92488a7
Eldar Goloviznin <radleweird@gmail.com>
2018-12-24 22:59:04 +0300

    removed redundant autogenerated tests

--- second_task/source/Package.swift
+++ second_task/source/Package.swift
@@ -16,9 +16,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "second_task",
-            dependencies: ["Socket", "SWXMLHash"]),
-        .testTarget(
-            name: "second_taskTests",
-            dependencies: ["second_task"]),
+            dependencies: ["Socket", "SWXMLHash"])
     ]
 )
--- second_task/source/Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import second_taskTests
-
-var tests = [XCTestCaseEntry]()
-tests += second_taskTests.allTests()
-XCTMain(tests)
