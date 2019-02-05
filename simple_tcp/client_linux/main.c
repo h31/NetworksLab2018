092fd37c5ab44595b97a74a01bca4b8e991aafe9
Nikolay Khlebnikov <Shadow_97@mail.ru>
2019-02-05 14:48:53 +0300

    Add files via upload

---
+++ simple_tcp/client_linux/main.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#include <string.h>
+
+
+ssize_t readn(int newsockfd, int n) {
+	ssize_t rbyte;
+	int abyte = 0;
+	char buffer[256];
+	
+	bzero(buffer, 256);
+	
+	while(rbyte < n) {
+		rbyte = read(newsockfd, buffer, n-rbyte < 255 ? n-rbyte : 255);
+		if (rbyte < 0) {
+			return (ssize_t)-1;
+		}
+		abyte = abyte + (int)rbyte;
+	    printf("%s\n", buffer);
+	}	
+	return (ssize_t)abyte;
+}
+
+int main(int argc, char *argv[]) {
+    int sockfd, n;
+    uint16_t portno;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[256];
+
+    if (argc < 3) {
+        fprintf(stderr, "usage %s hostname port\n", argv[0]);
+        exit(0);
+    }
+
+    portno = (uint16_t) atoi(argv[2]);
+
+    /* Create a socket point */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) {
+        perror("ERROR opening socket");
+        exit(1);
+    }
+
+    server = gethostbyname(argv[1]);
+
+    if (server == NULL) {
+        fprintf(stderr, "ERROR, no such host\n");
+        exit(0);
+    }
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy(server->h_addr, (char *) &serv_addr.sin_addr.s_addr, (size_t) server->h_length);
+    serv_addr.sin_port = htons(portno);
+
+    /* Now connect to the server */
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("ERROR connecting");
+        exit(1);
+    }
+
+    /* Now ask for a message from the user, this message
+       * will be read by server
+    */
+
+    printf("Please enter the message: ");
+    bzero(buffer, 256);
+    fgets(buffer, 255, stdin);
+
+    /* Send message to the server */
+    n = write(sockfd, buffer, strlen(buffer));
+
+    if (n < 0) {
+        perror("ERROR writing to socket");
+        exit(1);
+    }
+    
+    shutdown(sockfd, SHUT_WR);
+
+    /* Now read server response */
+    bzero(buffer, 256);
+    n = read(sockfd, buffer, 255);
+
+    if (n < 0) {
+        perror("ERROR reading from socket");
+        exit(1);
+    }
+    
+    close(sockfd);
+
+    printf("%s\n", buffer);
+    return 0;
+}
