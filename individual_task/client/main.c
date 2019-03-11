ac8a40d294dc40315f3dd48fedc3f5e9bf7303b0
Nikolay Khlebnikov <Shadow_97@mail.ru>
2019-03-12 00:42:44 +0300

    Update main.c

--- individual_task/client/main.c
+++ individual_task/client/main.c
@@ -1,14 +1,21 @@
-#include <windows.h>
-#include <ws2tcpip.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <locale.h>
 
 #include <string.h>
 
 #define BUF_SIZE 24
+#define INVALID_SOCKET -1
+#define SOCKET_ERROR -1
 
 #define ODD "38"
 #define EVEN "37"
+
+int money = 300;
 
 char *compareBet(char* bet) {
     char *id_client, *bet_client, *sum_client;
@@ -17,12 +24,12 @@ char *compareBet(char* bet) {
     bet_client = strtok(NULL, " ");
     sum_client = strtok(NULL, " ");
     if (strcmp(bet_client, ODD) == 0)
-        bet_client = "Odd";
+        bet_client = "Нечет";
     if (strcmp(bet_client, EVEN) == 0)
-        bet_client = "Even";
+        bet_client = "Чет";
     if (strcmp(bet_client, "0") == 0)
-        bet_client = "Zero";
-    char *result = malloc(18);
+        bet_client = "Зеро";
+    char *result = (char *)malloc(18);
     sprintf(result, "| %s | %s | %s |", id_client, bet_client, sum_client);
 
     return result;
@@ -33,11 +40,11 @@ int displayBets(int socket) {
     memset(buffer, 0, BUF_SIZE+1);
 
     write(socket, "GIVELIST", 8);
-    printf("| ID | BET  | SUM |\n");
+    printf("| ID | BET | SUM |\n");
     for(;;) {
         memset(buffer, 0, BUF_SIZE+1);
 
-        if (recv((SOCKET) socket, buffer, BUF_SIZE, 0) < 0) {
+        if (recv(socket, buffer, BUF_SIZE, 0) < 0) {
             perror("Read error");
             return -1;
         }
@@ -51,9 +58,9 @@ int displayBets(int socket) {
 
 int waitStartGame (int socket, int bet_count, int sum) {
     char buffer[BUF_SIZE];
-    printf("Waiting result.\n");
+    printf("Ожидание результата...\n");
     memset(buffer, 0, BUF_SIZE+1);
-    if (recv((SOCKET) socket, buffer, BUF_SIZE, 0) < 0) {
+    if (recv(socket, buffer, BUF_SIZE, 0) < 0) {
         return -1;
     }
     if (strstr(buffer, "RESULT") != NULL) {
@@ -62,14 +69,17 @@ int waitStartGame (int socket, int bet_count, int sum) {
         strncpy(bet, buffer+7, k - 7);
         if (bet_count == atoi(bet) || ((atoi(bet) % 2 == 1) && bet_count == 38)
                 || ((atoi(bet) % 2 == 0) && (atoi(bet) != 0) && bet_count == 37)) {
-            printf("You won.\n");
+            printf("Вы выиграли.\n");
             if (bet_count == 38 || bet_count == 37) {
-                printf("Your prize is %d.\n", sum * 2);
+                printf("Ваш приз - %d.\n", sum * 2);
+				money += sum * 2;
             } else {
-                printf("Your prize is %d.\n", sum * 36);
+                printf("Ваш приз - %d.\n", sum * 36);
+				money += sum * 36;
             }
+			printf("У Вас сейчас %d долларов.\n", money);
         } else {
-            printf("You lose.\n");
+            printf("Вы проиграли.\n");
         }
         return 0;
     }
@@ -79,7 +89,7 @@ int waitStartGame (int socket, int bet_count, int sum) {
 int checkSumBet() {
     char buffer[BUF_SIZE];
     for (;;) {
-        printf("Choose sum for your bet (from 1 to 100)\n");
+        printf("Выберите сумму ставки (от 1 до 99)\n");
         bzero(buffer, BUF_SIZE + 1);
         fgets(buffer, BUF_SIZE, stdin);
         strtok(buffer, "\n");
@@ -88,13 +98,17 @@ int checkSumBet() {
         strncpy(number, buffer, 3);
         long buf_int = strtol(number, &ptr, 10);
         if (ptr != number) {
-            if (buf_int < 1 || buf_int > 100) {
-                printf("Number must be from 1 to 100\n");
+            if (buf_int < 1 || buf_int > 99) {
+                printf("Значение должно быть от 1 до 99.\n");
                 continue;
             }
+			if (buf_int > money) {
+				printf("У Вас недостаточно денег. У Вас только %d долларов.\n", money);
+				continue;
+			}
             return (int) buf_int;
         } else {
-            printf("Number must be from 1 to 100\n");
+            printf("Значение должно быть от 1 до 99.\n");
         }
     }
 }
@@ -104,6 +118,8 @@ int sendBet(int socket, int bet) {
     int sum = checkSumBet();
     bzero(buffer, BUF_SIZE+1);
     sprintf(buffer, "%s %d %d", "MYBETIS", bet, sum);
+	money -= sum;
+	printf("У Вас сейчас %d долларов.\n", money);
     write(socket, buffer, 14);
     bzero(buffer, BUF_SIZE+1);
     read(socket, buffer, BUF_SIZE);
@@ -123,12 +139,12 @@ int welcomeGamer(int socket) {
         bzero(buffer, BUF_SIZE+1);
         read(socket, buffer, BUF_SIZE);
         if (strcmp(buffer, "GAMEPROCESS") == 0) {
-            printf("Game in process. Waiting...\n");
+            printf("Игра началась. Ожидание...\n");
             continue;
         }
         if (strcmp(buffer, "WELCOME") == 0) {
             for(;;) {
-                printf("Choose Action (type number):\n1.View list of bets;\n2.Place bet;\n3.Exit;\n");
+                printf("\nУ Вас сейчас %d долларов.\nВыберите действие:\n1.Просмотреть список ставок;\n2.Разместить ставку;\n3.Выйти;\n", money);
                 memset(buffer, 0, BUF_SIZE+1);
                 fgets(buffer, BUF_SIZE, stdin);
                 strtok(buffer, "\n");
@@ -139,18 +155,18 @@ int welcomeGamer(int socket) {
                 }
                 if (strcmp(buffer, "2") == 0) {
                     for(;;) {
-                        printf("Choose number from 0 to 36 or Even/Odd:\n");
+                        printf("Выберите число от 0 до 36 или чет/нечет:\n");
                         memset(buffer, 0, BUF_SIZE+1);
                         fgets(inside_buffer, BUF_SIZE, stdin);
                         strtok(inside_buffer, "\n");
 
-                        if (strcmp(inside_buffer, "Even") == 0) {
+                        if (strcmp(inside_buffer, "чет") == 0) {
                             if (sendBet(socket, 37) < 0) {
                                 return -1;
                             }
                             break;
                         }
-                        if (strcmp(inside_buffer, "Odd") == 0) {
+                        if (strcmp(inside_buffer, "нечет") == 0) {
                             if (sendBet(socket, 38) < 0) {
                                 return -1;
                             }
@@ -162,7 +178,7 @@ int welcomeGamer(int socket) {
                         long buf_int = strtol(number, &ptr, 10);
                         if (ptr != number) {
                             if (buf_int < 0 || buf_int > 36) {
-                                printf("Number must be from 0 to 36\n");
+                                printf("Значение должно быть от 0 до 36.\n");
                                 continue;
                             }
                             if (sendBet(socket, (int) buf_int) < 0) {
@@ -176,14 +192,14 @@ int welcomeGamer(int socket) {
                 if (strcmp(buffer, "3") == 0) {
                     bzero(buffer, BUF_SIZE+1);
                     write(socket, "EXIT", BUF_SIZE);
-                    printf("See you later. Bye-Bye!\n");
+                    printf("До новых встреч!\n");
                     bzero(buffer, BUF_SIZE+1);
                     read(socket, buffer, BUF_SIZE);
                     if (strcmp(buffer, "KICKED") == 0) {
                         close(socket);
                         return -1;
                     } else {
-                        printf("The server does not work correctly. The connection is terminated.\n");
+                        printf("Работа сервера некорректна. Соединение разорвано.\n");
                         close(socket);
                         return -1;
                     }
@@ -201,12 +217,12 @@ int welcomeCroupier(int socket) {
         bzero(buffer, BUF_SIZE+1);
         read(socket, buffer, BUF_SIZE);
         if (strcmp(buffer, "GAMEPROCESS") == 0) {
-            printf("Game in process. Waiting...\n");
+            printf("Игра началась. Ожидание...\n");
             continue;
         }
         if (strcmp(buffer, "WELCOME") == 0) {
             for(;;) {
-                printf("Choose Action (type number):\n1.View list of bets;\n2.Start game;\n3.Exit\n");
+                printf("Выберите действие:\n1. Просмотреть список ставок;\n2. Начать игру;\n3. Выйти\n");
                 memset(buffer, 0, BUF_SIZE+1);
                 fgets(buffer, BUF_SIZE, stdin);
                 strtok(buffer, "\n");
@@ -217,27 +233,27 @@ int welcomeCroupier(int socket) {
                 }
                 if (strcmp(buffer, "2") == 0) {
                     write(socket, "STARTGAME", BUF_SIZE);
-                    printf("Game is start. Wait result\n");
+                    printf("Игра началась. Ожидание результатов.\n");
                     bzero(buffer, BUF_SIZE+1);
                     read(socket, buffer, BUF_SIZE);
                     size_t k = strlen(buffer);
                     char *bet = malloc(k - 7);
                     strncpy(bet, buffer+7, k - 7);
-                    printf("Roulette win number - %d.\n", atoi(bet));
+                    printf("Выпавший номер на рулетке - %d.\n", atoi(bet));
 
                     break;
                 }
                 if (strcmp(buffer, "3") == 0) {
                     bzero(buffer, BUF_SIZE+1);
                     write(socket, "EXIT", BUF_SIZE);
-                    printf("See you later. Bye-Bye!\n");
+                    printf("До новых встреч!\n");
                     bzero(buffer, BUF_SIZE+1);
                     read(socket, buffer, BUF_SIZE);
                     if (strcmp(buffer, "KICKED") == 0) {
                         close(socket);
                         return -1;
                     } else {
-                        printf("The server does not work correctly. The connection is terminated.\n");
+                        printf("Работа сервера некорректна. Соединение разорвано.\n");
                         close(socket);
                         return -1;
                     }
@@ -252,17 +268,17 @@ int gamerMode(int socket) {
     char buffer[BUF_SIZE];
     memset(buffer, 0, BUF_SIZE+1);
 
-    recv((SOCKET) socket, buffer, BUF_SIZE, 0);
+    recv(socket, buffer, BUF_SIZE, 0);
     for(;;) {
         if (strcmp(buffer, "ONLYCROUP") == 0) {
-            printf("Table is full. We need croupier. Come back later.\n");
+            printf("Стол полон. Необходим крупье. Возвращайтесь позднее.\n");
             bzero(buffer, BUF_SIZE+1);
             read(socket, buffer, BUF_SIZE);
             if (strcmp(buffer, "KICKED") == 0) {
                 close(socket);
                 return -1;
             } else {
-                printf("The server does not work correctly. The connection is terminated.\n");
+                printf("Работа сервера некорректна. Соединение разорвано.\n");
                 close(socket);
                 return -1;
             }
@@ -271,7 +287,7 @@ int gamerMode(int socket) {
             size_t k = strlen(buffer);
             char *id = malloc(k - 8);
             strncpy(id, buffer+8, k - 8);
-            printf("Hi! Our Roulette for anonymous users. Your personal identification '%s'\n", id);
+            printf("Приветствуем! Наше казино соблюдает анонимность пользователей. Ваш персональный ID '%s'\n", id);
             return welcomeGamer(socket);
         }
     }
@@ -282,15 +298,15 @@ int croupMode(int socket) {
     char buf[BUF_SIZE];
     memset(buffer, 0, BUF_SIZE+1);
 
-    recv((SOCKET) socket, buffer, BUF_SIZE, 0);
+    recv(socket, buffer, BUF_SIZE, 0);
     for(;;) {
         if (strcmp(buffer, "CROUPEXIST") == 0) {
-            printf("Croupier already exist. Come back later.\n");
+            printf("Крупье уже есть. Возвращайтесь позднее.\n");
             close(socket);
             return -1;
         }
         if (strcmp(buffer, "PASW") == 0) {
-            printf("Password croupier: ");
+            printf("Пароль крупье: ");
             for(;;) {
                 memset(buffer, 0, BUF_SIZE+1);
                 fgets(buffer, BUF_SIZE, stdin);
@@ -300,15 +316,15 @@ int croupMode(int socket) {
                 bzero(buffer, BUF_SIZE+1);
                 read(socket, buffer, BUF_SIZE);
                 if (strcmp(buffer, "LOGGED") == 0) {
-                    printf("Login is successful.\n");
+                    printf("Вход успешен.\n");
                     return welcomeCroupier(socket);
                 }
                 if (strcmp(buffer, "WRONGPASW") == 0) {
-                    printf("Password is wrong:\nRetry: ");
+                    printf("Неправильный пароль.\nПовторите ввод: ");
                     continue;
                 }
                 if (strcmp(buffer, "THIRDPASW") == 0) {
-                    printf("Password is wrong. Sorry, comeback later.\n");
+                    printf("Неправильный пароль. Попробуйте позднее.\n");
                     return -1;
                 }
             }
@@ -319,7 +335,7 @@ int croupMode(int socket) {
 int chooseRole(int socket) {
     char buffer[BUF_SIZE]; //buffer
     for(;;) {
-        printf("Choose role (type number):\n1. Gamer\n2. Croupier\n");
+        printf("Выберите роль:\n1. Игрок\n2. Крупье\n");
         memset(buffer, 0, BUF_SIZE+1);
         fgets(buffer, BUF_SIZE, stdin);
         strtok(buffer, "\n");
@@ -331,14 +347,16 @@ int chooseRole(int socket) {
             write(socket, "CROUP", 5);
             return croupMode(socket);
         }
-        printf("No role found\n");
+        printf("Роль не найдена.\n");
     }
 }
 
 int main(int argc, char *argv[]) {
-    WSADATA wsaData;
-    int sockfd = INVALID_SOCKET, iresult;
-    struct addrinfo  *result = NULL, *ptr = NULL, hints;
+	setlocale(0, "Rus");
+    //WSADATA wsaData;
+    int sockfd/*INVALID_SOCKET*/, iresult;
+    struct addrinfo  *result = NULL, *ptr = NULL;
+	struct addrinfo hints;
 
     char buffer[BUF_SIZE];
 
@@ -347,11 +365,13 @@ int main(int argc, char *argv[]) {
         exit(0);
     }
 
+	/*
     iresult = WSAStartup(MAKEWORD(2,2), &wsaData);
     if (iresult != 0) {
         printf("WSAStartup failed with error: %d\n", iresult);
         return 1;
     }
+	*/
 
     memset( &hints, 0, sizeof(hints) );
     hints.ai_family = AF_UNSPEC;
@@ -361,23 +381,19 @@ int main(int argc, char *argv[]) {
     iresult = getaddrinfo(argv[1], argv[2], &hints, &result);
     if ( iresult != 0 ) {
         printf("getaddrinfo failed with error: %d\n", iresult);
-        WSACleanup();
         return 1;
     }
 
     for (ptr = result; ptr != NULL; ptr=ptr->ai_next) {
-        sockfd = (int) socket(ptr->ai_family, ptr->ai_socktype,
-                              ptr->ai_protocol);
+        sockfd = (int) socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
         if (sockfd == INVALID_SOCKET) {
-            printf("socket failed with error: %ld\n", (long) WSAGetLastError());
-            WSACleanup();
+	    printf("Извините, возникли ошибки:(");
             return 1;
         }
 
-        iresult = connect((SOCKET) sockfd, ptr->ai_addr, (int)ptr->ai_addrlen);
+        iresult = connect(sockfd, ptr->ai_addr, (int)ptr->ai_addrlen);
         if (iresult == SOCKET_ERROR) {
-            closesocket((SOCKET) sockfd);
-            sockfd = INVALID_SOCKET;
+            close(sockfd);
             continue;
         }
         break;
@@ -385,24 +401,17 @@ int main(int argc, char *argv[]) {
 
     freeaddrinfo(result);
 
-    if (sockfd == INVALID_SOCKET) {
-        printf("Unable to connect to server!\n");
-        WSACleanup();
-        return 1;
-    }
-
     /* Now ask for a message from the user, this message
        * will be read by server
     */
 
-    printf("Welcome to Azino777:\n");
+    printf("Добро пожаловать в Онлайн Казино:\n");
     memset(buffer, 0, BUF_SIZE+1);
     //fgets(buffer, BUF_SIZE, stdin);
     read(sockfd, buffer, BUF_SIZE);
     if (strcmp(buffer, "FULLTABLE") == 0) {
-        printf("Sorry, table is full. Come back later\n");
+        printf("Простите, стол полон. Возвращайтесь позднее.\n");
         close(sockfd);
-        WSACleanup();
         exit(0);
     }
     if (strcmp(buffer, "CONNECTED") == 0) {
